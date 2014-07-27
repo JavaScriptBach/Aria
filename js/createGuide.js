9efdcb4e7e028253614e612adb181e407b4dd7e1
Phillip Huang <phuang2@cs.washington.edu>
2014-07-27 17:01:48 +0800

    fixed html injection vulnerability, removed affix, and fixed NaN edge case

--- js/createGuide.js
+++ js/createGuide.js
@@ -4,6 +4,18 @@
 		console.log(this.value);
 	});
 
+	function escapeHTML(string) {
+		// var pre = document.createElement('pre');
+		// var text = document.createTextNode(string);
+		// pre.appendChild(text);
+		// return pre.innerHTML;
+		// return $("<pre>").text(string).html();
+		return string.replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+	};
+
 	$("#add-row-btn").click(function() {
 		var rows = $("tr");
 		var lastRow = rows.eq(rows.length - 1);
@@ -13,10 +25,12 @@
 			nextRowPageNumber = parseInt(lastCols[0].innerHTML) + 1;
 		else
 			nextRowPageNumber = parseInt(lastCols.eq(0).children().val()) + 1;
+		if (isNaN(nextRowPageNumber) || nextRowPageNumber < 1)
+			nextRowPageNumber = 1;
 		var nextRowStartTime = lastCols.eq(2).children().val();
-		var html = "<tr><td><input type='text' value='" + nextRowPageNumber + "'></td><td>";
-		html += "<input type='text' value='" + nextRowStartTime + "'></td>";
-		html += "<td><input type='text' value='foo'></td>";
+		var html = '<tr><td><input type="text" value="' + nextRowPageNumber + '"></td><td>';
+		html += '<input type="text" value="' + escapeHTML(nextRowStartTime) + '"></td>';
+		html += '<td><input type="text" value="foo"></td>';
 		$("tbody").append(html);
 	});
 
@@ -28,4 +42,6 @@
 			rows.eq(rows.length - 1).remove();
 		}
 	});
+
+	
 })();
