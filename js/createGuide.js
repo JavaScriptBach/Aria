65113ca9d9000a22fd431f921509607a5a7c6162
Phillip Huang <phuang2@cs.washington.edu>
2014-08-07 15:13:10 +0800

    added more controls in the create guide page

--- js/createGuide.js
+++ js/createGuide.js
@@ -49,25 +49,13 @@
 		});
 	});
 
-	function escapeHTML(string) {
-		// var pre = document.createElement('pre');
-		// var text = document.createTextNode(string);
-		// pre.appendChild(text);
-		// return pre.innerHTML;
-		// return $("<pre>").text(string).html();
-		return string.replace(/&/g, '&amp;')
-			.replace(/"/g, '&quot;')
-			.replace(/</g, '&lt;')
-			.replace(/>/g, '&gt;');
-	};
-
-	// Create a new table row, with intelligently filled in values
-	$("#add-row-btn").click(function() {
-		var rows = $("tr");
+	// Create a new table row for page data, with intelligently filled in values
+	$("#page-add-row-btn").click(function() {
+		var rows = $("#page-data tbody tr");
 		var lastRow = rows.eq(rows.length - 1);
 		var lastCols = lastRow.children();
 		var nextRowPageNumber;
-		if (rows.length == 2)
+		if (rows.length == 1)
 			nextRowPageNumber = parseInt(lastCols[0].innerHTML) + 1;
 		else
 			nextRowPageNumber = parseInt(lastCols.eq(0).children().val()) + 1;
@@ -77,17 +65,31 @@
 		var html = '<tr><td><input type="text" value="' + nextRowPageNumber + '"></td><td>';
 		html += '<input type="text" value="' + escapeHTML(nextRowStartTime) + '"></td>';
 		html += '<td><input type="text" value="foo"></td>';
-		$("tbody").append(html);
+		$("#page-data tbody").append(html);
 	});
 
-	// Removes the bottom-most table row after confirmation
-	$("#remove-row-btn").click(function() {
-		var rows = $("tr");
-		if (rows.length == 2)
+	// Removes the bottom-most table row for page data after confirmation
+	$("#page-remove-row-btn").click(function() {
+		removeRow("page");
+	});
+
+	// Removes the bottom-most table row for guide data after confirmation
+	$("#guide-remove-row-btn").click(function() {
+		removeRow("guide");
+	});
+
+	function removeRow(type) {
+		var rows = $("#" + type + "-data tbody tr");
+		if (rows.length == 1)
 			return;
-		if (confirm("Are you sure you want to delete the last row?")) {
+		if (confirm("Are you sure you want to delete the last row?"))
 			rows.eq(rows.length - 1).remove();
-		}
-	});
+	}
 
+	function escapeHTML(string) {
+		return string.replace(/&/g, '&amp;')
+			.replace(/"/g, '&quot;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;');
+	}
 })();
